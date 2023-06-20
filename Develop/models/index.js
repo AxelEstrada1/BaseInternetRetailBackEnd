eb79ea0724c0c0d3235e4b1b951f22f0e93f281a
Axel <axelestrada1890@gmail.com>
2023-06-20 06:13:07 -0700

    added db js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,25 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  onDelete: 'SET NULL'
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  onDelete: 'SET NULL'
+});
 
 module.exports = {
   Product,
